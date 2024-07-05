da62bdf28fc2c8572b509a840411a5bcec202961
Hugo Torres <hogotoluca@hotmail.com>
2024-07-05 00:02:42 -0600

    Primer Commit

---
+++ src/config/envs.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import * as Joi from 'Joi';
+
+interface EnvVars {
+  PORT: number;
+  DATABASE_URL: string;
+}
+
+const envsSchema = Joi.object({
+  PORT: Joi.number().required(),
+  DATABASE_URL: Joi.string().required(),
+}).unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+  databaseUrl: envVars.DATABASE_URL,
+};
